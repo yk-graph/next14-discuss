8b32e1d8ef2fd6a7bad54699852da7fe13e3553c
yk-graph <yokokura.tatsuya@gmail.com>
2023-12-31 16:34:37 +0900

    add post topic in error handling

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,89 @@
-'use server';
+/*
+  [Tips] zodを使ってバリデーションを行う方法
+  1. バリデーションルールのスキーマを定義する
+  2. バリデーションルールを使ってバリデーションを行う -> safeParse でバリデーションを行い、その結果を result に格納する
+  3. result.success が true ならバリデーション成功、false ならバリデーション失敗
+  4. バリデーション失敗時は result.error.flatten().fieldErrors からエラー内容を取得する
+*/
 
-export async function createTopic() {
-  // TODO: revalidate the homepage
+"use server";
+
+import type { Topic } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+import { auth } from "@/lib/auth";
+import { db } from "@/lib/db";
+import paths from "@/helpers/paths";
+
+// [Tips] バリデーションのスキーマを定義するのは server action のコード内で定義しておく
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/[a-z-]/, {
+      message: "Must be lowercase letters or dashes without spaces",
+    }),
+  description: z.string().min(10),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must be signed in to do this."],
+      },
+    };
+  }
+
+  let topic: Topic;
+  try {
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ["Something went wrong"],
+        },
+      };
+    }
+  }
+
+  revalidatePath("/");
+  redirect(paths.topicShow(topic.slug));
 }
--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,7 +1,16 @@
 /*
   [Tips] server action を使ってリクエストを送信するために form の action 属性に処理を指定する
+  1. useFormState で formState と action を取得する
+  2. formState は errors と values を持つ
+  3. errors は field ごとに errors を持つ
+  4. action は useFormState の第一引数に指定した server action を実行する関数（今回の場合は action = actions.createTopic 関数に該当する）
+  5. form の action属性 に対して useFormState で定義した action を指定することで、form の submit 時に action が実行される
+  6. 渡す値は input の name 属性に対応する
 */
 
+"use client";
+
+import { useFormState } from "react-dom";
 import {
   Input,
   Button,
@@ -13,21 +22,43 @@ import {
 import * as actions from "@/actions";
 
 export default function TopicCreateForm() {
+  const [formState, action] = useFormState(actions.createTopic, {
+    errors: {},
+  });
+
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button color="primary">Create a Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action={actions.createTopic}>
+        <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Topic</h3>
-            <Input label="Name" labelPlacement="outside" placeholder="Name" />
+            <Input
+              name="name"
+              label="Name"
+              labelPlacement="outside"
+              placeholder="Name"
+              isInvalid={!!formState.errors.name}
+              errorMessage={formState.errors.name?.join(", ")}
+            />
+
             <Textarea
+              name="description"
               label="Description"
               labelPlacement="outside"
               placeholder="Describe your topic"
+              isInvalid={!!formState.errors.description}
+              errorMessage={formState.errors.description?.join(", ")}
             />
+
+            {formState.errors._form ? (
+              <div className="rounded p-2 bg-red-200 border border-red-400">
+                {formState.errors._form?.join(", ")}
+              </div>
+            ) : null}
+
             <Button type="submit">Submit</Button>
           </div>
         </form>
